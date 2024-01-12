965e1188428a4a20f09f7a3f3a86e4e0ca5f616b
vinoth-Kumar-S <31274282+vinoth-Kumar-S@users.noreply.github.com>
2024-01-12 12:20:15 +0530

    Adding Core DataGrid Sorting sample
    Adding Core DataGrid Sorting sample

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Dynamic;
+namespace DataGrid.Pages
+{
+    public class OrderDetails
+    {
+        public static List<OrderDetails> order = new List<OrderDetails>();
+
+        public OrderDetails(int OrderID, string CustomerId, int EmployeeId, double Freight,
+            DateTime OrderDate, string ShipCity, string status)
+        {
+            this.OrderID = OrderID;
+            this.CustomerID = CustomerId;
+            this.EmployeeID = EmployeeId;
+            this.Freight = Freight;
+            this.ShipCity = ShipCity;
+            this.OrderDate = OrderDate;
+            this.Status = status;
+        }
+
+        public static List<OrderDetails> GetAllRecords()
+        {
+            if (order.Count() == 0)
+            {
+                int code = 100;
+                for (int i = 1; i < 3; i++)
+                {
+                    order.Add(new OrderDetails(code + 1, "BLONP", i + 0, 2.3 * i, new DateTime(2023, 05, 15), "Berlin", "Order Received"));
+                    order.Add(new OrderDetails(code + 2, "ANATR", i + 2, 3.3 * i, new DateTime(2022, 04, 04), "Madrid", "Shipped"));
+                    order.Add(new OrderDetails(code + 3, "JOE", i + 1, 4.3 * i, new DateTime(2023, 11, 30), "Cholchester", "Out for Delivery"));
+                    order.Add(new OrderDetails(code + 4, "ALFKI", i + 3, 5.3 * i, new DateTime(2021, 10, 22), "Marseille", "Delivered"));
+                    order.Add(new OrderDetails(code + 5, "BOLID", i + 4, 6.3 * i, new DateTime(2020, 02, 18), "Tsawassen", "Out for Delivery"));
+                    order.Add(new OrderDetails(code + 6, "MICHAEL", i + 4, 6.3 * i, new DateTime(2021, 02, 18), "Berlin", "Shipped"));
+                    order.Add(new OrderDetails(code + 7, "DANIEL", i + 4, 6.3 * i, new DateTime(2022, 02, 18), "Berlin", "Delivered"));
+                    order.Add(new OrderDetails(code + 8, "RICHARD", i + 4, 6.3 * i, new DateTime(2023, 02, 18), "Berlin", "Shipped"));
+                    order.Add(new OrderDetails(code + 9, "ANTON", i + 4, 6.3 * i, new DateTime(2022, 02, 18), "Berlin", "Out for Delivery"));
+                    code += 9;
+                }
+            }
+            return order;
+        }
+
+        public int OrderID { get; set; }
+        public string CustomerID { get; set; }
+        public int? EmployeeID { get; set; }
+        public double? Freight { get; set; }
+        public string ShipCity { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public string Status { get; set; }
+
+    }
+
+    public class IndexModel : PageModel
+    { 
+        public void OnGet()
+        {
+           
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# how-to-sort-data-in-the-aspdotnet-core-datagrid-control
-A quick-start project that helps to sort data in the Syncfusion ASP.NET Core DataGrid control. This project also contains code to enable and implement different types of sorting features, including single column sorting, multicolumn sorting, and the custom sort comparer.
+# How to Sort Data in the ASP.NET Core DataGrid Control
+
+A quick-start project that helps to sort data in the Syncfusion ASP.NET Core DataGrid control. This project includes code to enable and implement various sorting features, such as single-column sorting, multicolumn sorting, and the use of a custom sort comparer.
+
+Sorting documentation for the Syncfusion ASP.NET Core DataGrid control: 
+https://ej2.syncfusion.com/aspnetcore/documentation/grid/sorting  
+
+Touch interaction documentation for Sorting in the Syncfusion ASP.NET Core DataGrid control: 
+https://ej2.syncfusion.com/aspnetcore/documentation/grid/sorting#touch-interaction 
+
+Check out this online example of a multi-sorting in the Syncfusion ASP.NET Core DataGrid control:
+https://ej2.syncfusion.com/aspnetcore/Grid/MultiSorting#/fluent 
+
+Watch this video to get started with the ASP.NET Core DataGrid control:
+https://www.youtube.com/watch?v=E5w2fO-N9gE  
+
+Tutorial video: https://www.syncfusion.com/tutorial-videos 
+
+## Project pre-requisites
+
+Make sure that you have the compatible versions of Visual Studio 2022 and .NET 7.0 SDK framework in your machine before starting to work on this project.
+
+### How to run this application?
+
+To run this application, you need to clone the `how-to-sort-data-in-the-aspdotnet-core-datagrid-control` repository and then open it in Visual Studio 2022. Now, simply run the project and by pressing the `F5` command.
